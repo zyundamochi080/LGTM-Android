e519474efb6c0158701b605639de1dec40d80611
KG <kg.vitaty14@gmail.com>
2020-04-06 18:28:29 +0900

    Add preview via Camera2

--- app/src/main/java/vitaty14/kg/lgtm_android/MainActivity.kt
+++ app/src/main/java/vitaty14/kg/lgtm_android/MainActivity.kt
@@ -1,7 +1,18 @@
 package vitaty14.kg.lgtm_android
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Surface
+import android.view.TextureView
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +20,73 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (SurfaceView.isAvailable) {
+            openCamera()
+        } else {
+            SurfaceView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+                override fun onSurfaceTextureAvailable(p0: SurfaceTexture?, p1: Int, p2: Int) {
+                    openCamera()
+                }
+                override fun onSurfaceTextureSizeChanged(texture: SurfaceTexture?, p1: Int, p2: Int) {}
+                override fun onSurfaceTextureUpdated(texture: SurfaceTexture?) {}
+                override fun onSurfaceTextureDestroyed(texture: SurfaceTexture?): Boolean = true
+            }
+        }
+    }
+
+    private var cameraDevice: CameraDevice? = null
+
+    private val cameraManager: CameraManager by lazy {
+        getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    private fun openCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        cameraManager.openCamera("0", object: CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+                createCameraPreviewSession()
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                cameraDevice?.close()
+                cameraDevice = null
+            }
+
+            override fun onError(camera: CameraDevice, p1: Int) {
+                cameraDevice?.close()
+                cameraDevice = null
+            }
+        }, null)
+    }
+
+    private var captureSession: CameraCaptureSession? = null
+
+    private fun createCameraPreviewSession() {
+        if (cameraDevice == null) {
+            return
+        }
+        val texture = SurfaceView.surfaceTexture
+        texture.setDefaultBufferSize(640, 480)
+        val surface = Surface(texture)
+
+        val previewRequestBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        previewRequestBuilder.addTarget(surface)
+
+        cameraDevice?.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                captureSession = session
+                captureSession?.setRepeatingRequest(previewRequestBuilder.build(), null, null)
+            }
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {}
+        }, null)
+    }
+
 }
